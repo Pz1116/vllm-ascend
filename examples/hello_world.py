265d223afdbda7019c258e0364b2b83297f10a02
Pz1116 <zpbzpb123123@gmail.com>
2026-03-09 10:51:08 +0800

    Add hello world demo example
    - Simple Python script to verify basic setup
    - Created by Claw as a demo
    
    Signed-off-by: Pz1116 <zpbzpb123123@gmail.com>

---
+++ examples/hello_world.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Hello World demo for vllm-ascend.
+
+A simple example to verify the basic setup works.
+"""
+
+
+def main():
+    print("Hello, vllm-ascend! 🚀")
+    print("This is a demo file created by Claw.")
+    print("If you can see this, your Python environment is working!")
+
+
+if __name__ == "__main__":
+    main()
