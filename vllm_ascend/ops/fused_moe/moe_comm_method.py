b135bb7837c9df050964afacb7cbda48668adf30
GDzhu01 <809721801@qq.com>
2026-04-20 11:22:47 +0800

    fusemc2 with clamp
    Signed-off-by: GDzhu01 <809721801@qq.com>

--- vllm_ascend/ops/fused_moe/moe_comm_method.py
+++ vllm_ascend/ops/fused_moe/moe_comm_method.py
@@ -296,6 +296,7 @@ class FusedMC2CommImpl(MoECommMethod):
                 probs=fused_experts_input.topk_weights.to(torch.float32),
                 group=self.token_dispatcher.moe_all_to_all_group_name,
                 max_output_size=65536,
+                swiglu_limit=10.0,
                 out=out,
                 expert_token_nums=self.expert_token_nums,
             )
